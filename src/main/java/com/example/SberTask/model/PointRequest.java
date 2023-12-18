a043adaa479c6aa5cb194681f11c45576b8de80b
InsoFessDev <146421463+InsoFessDev@users.noreply.github.com>
2023-12-18 19:16:07 +0300

    Update PointRequest.java

--- src/main/java/com/example/SberTask/model/PointRequest.java
+++ src/main/java/com/example/SberTask/model/PointRequest.java
@@ -1,2 +1,30 @@
-package com.example.SberTask.model;public class PointRequest {
+package com.example.SberTask.model;
+
+public class PointRequest {
+    private Long clientId;
+    private int amount;
+
+    public PointRequest() {
+    }
+
+    public PointRequest(Long clientId, int amount) {
+        this.clientId = clientId;
+        this.amount = amount;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
