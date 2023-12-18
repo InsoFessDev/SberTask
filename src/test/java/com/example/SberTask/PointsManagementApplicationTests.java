f3c75758d7ce20bfec51fbfcf4008f8a5814da7e
InsoFessDev <146421463+InsoFessDev@users.noreply.github.com>
2023-12-18 19:23:05 +0300

    Update PointsManagementApplicationTests.java

--- src/test/java/com/example/SberTask/PointsManagementApplicationTests.java
+++ src/test/java/com/example/SberTask/PointsManagementApplicationTests.java
@@ -1,13 +1,56 @@
-package com.example.SberTask;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class PointsManagementApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testAddPoints() {
+
+		ClientRepository clientRepository = Mockito.mock(ClientRepository.class);
+
+		// Создаем объект сервиса и передаем в него мок репозитория
+		PointService pointService = new PointServiceImpl(clientRepository);
+
+		// Создаем тестового клиента
+		Client client = new Client();
+		client.setId(1L);
+		client.setName("John Doe");
+		client.setPoints(100);
+
+		// Кол-во баллов, которые мы хотим добавить
+		int amount = 50;
+
+		// Метод addPoints должен вызывать соответствующий метод репозитория и
+		// увеличивать баллы клиента на указанное кол-во
+		Mockito.when(clientRepository.findById(1L)).thenReturn(java.util.Optional.of(client));
+		pointService.addPoints(1L, amount);
+
+		// Проверяем, что баллы клиента были увеличены на указанное кол-во
+		assertEquals(150, client.getPoints());
+	}
+
+	@Test
+	public void testDeductPoints() {
+		// Создаем мок объект репозитория
+		ClientRepository clientRepository = Mockito.mock(ClientRepository.class);
+
+		// Создаем объект сервиса и передаем в него мок репозитория
+		PointService pointService = new PointServiceImpl(clientRepository);
+
+		// Создаем тестового клиента
+		Client client = new Client();
+		client.setId(1L);
+		client.setName("John Doe");
+		client.setPoints(100);
+
+		// Кол-во баллов, которые мы хотим списать
+		int amount = 50;
+
+		// Метод deductPoints должен вызывать соответствующий метод репозитория и
+		// уменьшать баллы клиента на указанное кол-во
+		Mockito.when(clientRepository.findById(1L)).thenReturn(java.util.Optional.of(client));
+		pointService.deductPoints(1L, amount);
+
+		// Проверяем, что баллы клиента были уменьшены на указанное кол-во
+		assertEquals(50, client.getPoints());
 	}
 
 }
